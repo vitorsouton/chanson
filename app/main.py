e70d1de7341aa7ed5c652452648b4d8b807b91b5
Vitor Neves <vitor.sneves@hotmail.com>
2022-05-12 14:48:55 -0300

    sdjkasdhsa

--- app/main.py
+++ app/main.py
@@ -77,3 +77,8 @@ def done():
 
     return render_template('playlists.html',
                            user_id=user_id)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
